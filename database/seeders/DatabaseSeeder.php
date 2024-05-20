aedd16938a8e8e64c79b255aad4f4f4c772fe08e
maksim.halim <maksim.halim@gmail.com>
2024-05-20 21:35:08 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Categories;
+use App\Models\Posts;
+use App\Models\Comments;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Categories::factory(50)->create();
+        Posts::factory(2000)->create()->each(function ($post) {
+            Comments::factory(3)->create(['post_id' => $post->id]);
+        });
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
